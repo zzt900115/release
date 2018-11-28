d5f4b2fa4e7a14a37fdde6a58645ead2e56b5935
zzt900115 <zzt900115@163.com>
2018-11-29 00:49:39 +0800

    Initial commit

---
+++ src/main/java/com/didi/release/ReleaseApplication.java
@@ -0,0 +1,12 @@
+package com.didi.release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReleaseApplication.class, args);
+    }
+}
