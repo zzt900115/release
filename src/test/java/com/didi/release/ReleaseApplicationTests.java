6ac3fdfe79ef7bf3a4f473043d71f470c8839b97
zhaozhentian <zhaozhentian@didichuxing.com>
2018-12-10 00:17:43 +0800

    删除无用的文件

--- src/test/java/com/didi/release/ReleaseApplicationTests.java
+++ src/test/java/com/didi/release/ReleaseApplicationTests.java
@@ -1,7 +1,11 @@
 package com.didi.release;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ReleaseApplicationTests {
 
+    public Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void jsonArrayTest(){
+        JSONArray array1 = JSON.parseArray("");
+        JSONArray array2 = JSON.parseArray("[]");
+
+        log.info(array1.toJSONString());
+        log.info(array2.toJSONString());
+
     }
 
 }
